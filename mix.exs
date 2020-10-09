1da770a6cb6608a440ccbc5e3210cc38b8972fd0
Andrey N. Ronin <anronin@gmail.com>
2020-10-09 11:10:32 +0400

    Bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MessageQueue.MixProject do
   use Mix.Project
 
   @name "MessageQueue"
-  @version "0.1.1"
+  @version "0.1.2"
   @repo_url "https://github.com/ChannexIO/message_queue"
 
   def project do
