d4feb43e7b1d28e2994413a4ae913087940f96d3
Andrey N. Ronin <anronin@gmail.com>
2021-10-07 01:45:24 +0400

    Release v0.5.0

--- lib/message_queue/application.ex
+++ lib/message_queue/application.ex
@@ -4,11 +4,6 @@ defmodule MessageQueue.Application do
   use Application
 
   def start(_type, _args) do
-    :logger.add_primary_filter(
-      :ignore_rabbitmq_progress_reports,
-      {&:logger_filters.domain/2, {:stop, :equal, [:progress]}}
-    )
-
     children = [
       MessageQueue.Connection,
       MessageQueue.Producer,
