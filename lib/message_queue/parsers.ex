66b1e6405a97c100960980727043591f30c2eea7
Andrey N. Ronin <anronin@gmail.com>
2021-10-13 14:30:46 +0400

    Add Message and parsers

---
+++ lib/message_queue/parsers.ex
@@ -0,0 +1,19 @@
+defmodule MessageQueue.Parsers do
+  @moduledoc false
+
+  alias __MODULE__.{ExtBinary, Json}
+
+  @doc false
+  def encode(message) do
+    get_parser(message).encode(message.data, message.parser_opts)
+  end
+
+  @doc false
+  def decode(message) do
+    get_parser(message).decode(message.data, message.parser_opts)
+  end
+
+  defp get_parser(%{type: :json}), do: Json
+  defp get_parser(%{type: :ext_binary}), do: ExtBinary
+  defp get_parser(%{type: _}), do: Json
+end
