34d6db224fb9c7e04639c682ed2cd00c039b59f7
Andrew Yudin <pasarian@gmail.com>
2024-05-31 18:33:30 +0200

    Release v0.8.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MessageQueue.MixProject do
   use Mix.Project
 
   @name "MessageQueue"
-  @version "0.7.0"
+  @version "0.8.0"
   @repo_url "https://github.com/ChannexIO/message_queue"
 
   def project do
